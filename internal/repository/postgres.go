ed094f73be2a8b5dd82aa78e2efb4826c97e0fae
Makar <makarkudryavtsev@mail.ru>
2025-09-01 20:17:14 +0300

    fix: new method to found path to migrations

--- internal/repository/postgres.go
+++ internal/repository/postgres.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"fmt"
 	"log"
+	"path/filepath"
 
 	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
@@ -10,7 +11,12 @@ import (
 )
 
 func RunMigrations(dsn string) error {
-	m, err := migrate.New("file://../../migrations/", dsn)
+	projectRoot := filepath.Join("..", "..")
+	absPath, err := filepath.Abs(filepath.Join(projectRoot, "migrations"))
+	if err != nil {
+		return fmt.Errorf("failed to get absolute path: %v", err)
+	}
+	m, err := migrate.New("file://"+absPath, dsn)
 	if err != nil {
 		return fmt.Errorf("failed to init migrations: %v", err)
 	}
