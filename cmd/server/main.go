8c096ea7c16597ecbdd08860ab32b6ab243cf0ff
Makar <makarkudryavtsev@mail.ru>
2025-11-19 05:01:26 +0300

    docs: remove OpenAPI metadata annotations from main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,18 +4,6 @@
 //   - gauge: датчики (значения устанавливаются)
 //   - counter: счётчики (значения увеличиваются)
 //
-// @title Metrics Alerter API
-// @version 1.0
-// @description REST API для сбора и хранения метрик системы
-// @termsOfService http://swagger.io/terms/
-//
-// @contact.name API Support
-// @contact.url http://www.example.com/support
-// @contact.email support@example.com
-//
-// @license.name Apache 2.0
-// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
-//
 // @host localhost:8080
 // @BasePath /
 //
